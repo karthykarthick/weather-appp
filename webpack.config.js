b1bf75809be78caf68f66d01724fc8a028adebd7
karthick <karthylock@gmail.com>
2021-02-10 12:58:05 +0530

    added linters and required files

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
