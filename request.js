877bdb47ab5e769aaecce7724403876bc484c3a5
karthick <karthylock@gmail.com>
2021-02-11 23:18:58 +0530

    added files

--- request.js
+++ request.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const key = 'cbe3dd267a18f6c89943b3eff94f1ed7';
 
 // const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
@@ -9,7 +10,7 @@ const key = 'cbe3dd267a18f6c89943b3eff94f1ed7';
 //     });
 
 const requestCity = async (city) => {
-    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
     const query = `?q=${city}&appid=${key}`;
 
     //make fetch call (promise call)
